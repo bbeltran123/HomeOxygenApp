6dc3d6956a53cbab01b47dfadb35677910189cb2
bbeltran1234 <bbeltran@stanford.edu>
2020-07-18 14:20:10 -0700

    restart

--- ios/poledialink.swift
+++
@@ -1,9 +0,0 @@
-//
-//  poledialink.swift
-//  BLEDiscoveryApp
-//
-//  Created by Gregg Larson on 2/23/20.
-//  Copyright © 2020 Facebook. All rights reserved.
-//
-
-import Foundation
--- reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import BLEReducer from './BLEReducer';
-
-export default combineReducers({
-    BLEs: BLEReducer,
-  });
